71df24a93995950eb0ce236233b0968bb1ac90cf
tony555t <tonytullah764@gmail.com>
2022-12-20 21:35:13 +0300

    code cleaning

--- js/index.js
+++ js/index.js
@@ -28,28 +28,3 @@ const options = {
   
 
   
-  // fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
- 
-
-// fetch('https://kfc-chickens.p.rapidapi.com/chickens', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-// fetch('https://kfc-chickens.p.rapidapi.com/chickens',options )
-
-//   .then(response => response.json())
-//   .then(data => {
-//     // data is a JSON object containing the list of menu items
-//     data.products.forEach(product => {
-//       // create a new list item for each menu item
-//       var item = document.createElement('li');
-//       item.innerHTML = `<h3>${product.name}</h3><p>${product.description}</p>`;
-//       // add the list item to the menu items list
-//       document.getElementById('menu-items').appendChild(item);
-//     });
-//   })
-//   .catch(error => console.error(error));
