e98c0876dbf41742e92b1d57b8950cbc705dbad3
tony555t <tonytullah764@gmail.com>
2022-12-20 21:04:20 +0300

    styling

--- js/index.js
+++ js/index.js
@@ -1,56 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-
-  // ROWS DATA
-  const randomMealRow = document.getElementById('random-meal')
-  const mealCategoryRow = document.getElementById('menu')
-  const searchRow = document.getElementById('search-result')
-
-  // LINKS DATA
-  const menuLink = document.getElementById('menu-link')
-  const homeLink = document.getElementById('home-link')
-
-  // search form
-  const searchForm = document.getElementById('search-form')
-  const searchInput = document.getElementById('search')
-
-
-    // CLICK EVENTS FOR LINKS
-      menuLink.addEventListener('click', () => {
-      // hide random meal
-      menu.style.display = "none"
-      // hide search page
-      searchRow.style.display = "none"
-      // show categories
-      mealCategoryRow.removeAttribute('hidden')
-      mealCategoryRow.style.display = "flex"
-
-  })
-
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '43a42eada7msh7e57818917bec6dp11390ejsn707f8b964a64',
-		'X-RapidAPI-Host': 'kfc-chickens.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '43a42eada7msh7e57818917bec6dp11390ejsn707f8b964a64',
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+  }
 };
+
+  fetch("https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes", options)
+.then(response => response.json())
+.then(data => {
+  console.log(data)
+  data.results.forEach((result, index) => {
+    const card = document.getElementById(`card-${index + 1}`);
+    console.log(card)
+    const rname = result["name"]
+    console.log(rname)
+    // const speciesImage = species["Species Illustration Photo"].src
+    card.querySelector(".species-image").src = result.thumbnail_url;
+    card.querySelector(".name").innerHTML = rname;
+    card.querySelector(".description").innerHTML = `${result.description}`;
+    card.querySelector(".price").innerHTML = `price: ${result.price.total}`;
+
+  });
+});
+
+   
+  
+
+  
+  // fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
+  //   .then(response => response.json())
+  //   .then(response => console.log(response))
+  //   .catch(err => console.error(err));
+ 
 
 // fetch('https://kfc-chickens.p.rapidapi.com/chickens', options)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-fetch('https://kfc-chickens.p.rapidapi.com/chickens',options )
+// fetch('https://kfc-chickens.p.rapidapi.com/chickens',options )
 
-  .then(response => response.json())
-  .then(data => {
-    // data is a JSON object containing the list of menu items
-    data.products.forEach(product => {
-      // create a new list item for each menu item
-      var item = document.createElement('li');
-      item.innerHTML = `<h3>${product.name}</h3><p>${product.description}</p>`;
-      // add the list item to the menu items list
-      document.getElementById('menu-items').appendChild(item);
-    });
-  })
-  .catch(error => console.error(error));
+//   .then(response => response.json())
+//   .then(data => {
+//     // data is a JSON object containing the list of menu items
+//     data.products.forEach(product => {
+//       // create a new list item for each menu item
+//       var item = document.createElement('li');
+//       item.innerHTML = `<h3>${product.name}</h3><p>${product.description}</p>`;
+//       // add the list item to the menu items list
+//       document.getElementById('menu-items').appendChild(item);
+//     });
+//   })
+//   .catch(error => console.error(error));
